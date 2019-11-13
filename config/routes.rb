ba164049d98340dacbac306882dc80d11319ca11
John Schults <johnschults@hotmail.com>
2019-11-13 22:25:58 +0100

    Updating YLP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews
+
+  #get 'restaurants', to: 'restaurants#index'
+  #get 'restaurants/new', to: 'restaurants#new'
+  #get 'restaurants/:id', to: 'restaurants#show'
+  #get 'restaurants/:id/edit', to: 'restaurants#edit'
+  #patch 'restaurants/:id', to: 'restaurants#update'
+  #delete 'restaurants/:id', to: 'restaurants#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
